817c977bedb00aa261a19270e2be0d2542a8c8a1
Andrew Perotti <Perotti28@gmail.com>
2024-11-21 09:13:27 -0800

    Add pseudo code for project
    This will slowly be replaced with functional and appropriately named
    functions and variables.

---
+++ main.js
@@ -0,0 +1,22 @@
+//create functions for operations: add, subtract, multiply, divide
+
+
+//create an 'operate' function that takes in 3 arguments: 2 integers and 1 operator, and this function will call the appropriate earlier declared function depending on the operator given.
+
+//create functions that run the operations (listeners) and place the results into the display of the calculator. 
+//Don't forget to save the output as a variable as it will be usable with any proceeding operations. ex 1+1-2, 1+1 needs to be solved and displayed as they hit + and then 2 and then that total needs to display
+//Each time operate is called === update the display to the user.
+
+//round long decimals to a point so they don't overflow the display
+
+//disallow pressing = out of order
+
+//clear button should wipe everything done so far
+
+//if the user tries to divide by 0, display to them a snarky message
+
+
+
+//extra credit: add a decimal button that allows users to create floating point numbers, but don't allow more than one
+    //add a backspace button, so users can undo their last input 
+//add keyboard support
