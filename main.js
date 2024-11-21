3651bf7e6ad851f4e1ed11233211ce8c1df30b40
Andrew Perotti <Perotti28@gmail.com>
2024-11-21 10:13:29 -0800

    Add variables to button selectors
    I created variables that point to each button in the DOM which will,
    however I'm certain I can delegate the listener to their parent and then
     dynamically figure out what buttons are pressed using e.target, I will
     test that next.

--- main.js
+++ main.js
@@ -35,6 +35,31 @@ let currentTotal = 0;
 resultDisplay.textContent=currentTotal
 
 let currentInput = null;
+
+let clearButton = document.querySelector('#clearButton')
+let signButton = document.querySelector('#signButton')
+let modulosButton = document.querySelector('#modulosButton')
+let divideButton = document.querySelector('#divideButton')
+let sevenButton = document.querySelector('#sevenButton')
+let eightButton = document.querySelector('#eightButton')
+let nineButton = document.querySelector('#nineButton')
+let multiplyButton = document.querySelector('#multiplyButton')
+let fourButton = document.querySelector('#fourButton')
+let fiveButton = document.querySelector('#fiveButton')
+let sixButton = document.querySelector('#sixButton')
+let subtractButton = document.querySelector('#subtractButton')
+let oneButton = document.querySelector('#oneButton')
+let twoButton = document.querySelector('#twoButton')
+let threeButton = document.querySelector('#threeButton')
+let addButton = document.querySelector('#addButton')
+let backButton = document.querySelector('#backButton')
+let decimalButton = document.querySelector('#decimalButton')
+let zeroButton = document.querySelector('#zeroButton')
+let operateButton = document.querySelector('#operateButton')
+
+
+
+
 //Each time operate is called === update the display to the user.
 
 //round long decimals to a point so they don't overflow the display
