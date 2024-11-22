28ff94b427fe90df3100acef1bb016620e057951
Andrew Perotti <Perotti28@gmail.com>
2024-11-22 08:45:37 -0800

    Add pseudo code that explains what your js needs
    I'm hoping that how I've written this will help me decipher what I need
    to do before I write code, however I may edit this again before
    continuing writing any code.

---
+++ main.js
@@ -0,0 +1,23 @@
+//create functions that mimic the basic math operators a calculator contains; add, subtract, multiply, and divide
+
+//Create three variables for the two integer inputs and the operator
+
+//Create an 'operate' function that takes those three variables and invokes one of the basic functions from above, this will also use the operation selected by the user to pick which of the functions from above will invoke. Update the display with the result of the invoked function.
+
+//Create a function to clear everything
+
+//Create a function that populate the display, and apply it to when the user clicks on digit buttons.
+
+//Create a variable that stores the previous operation/operate total
+
+
+//ec:
+//decimal button for making floating point integers, disallow the use of it when it has already been used
+//add a backspace button so the user can undo their last input if they clicked the wrong number than they wanted
+//add keyboard support
+
+//notes
+//round answers with long decimals so they don't overflow the display
+//disallow pressing of the = when it doesn't make sense to
+//display a snarky error message if the user tries to divide by 0, and of course disallow that from working
+
